ae712817dd969f639cb2c3b534a7766c55ca9924
Husa221 <dan.pluhar@seznam.cz>
2024-10-05 15:08:51 +0200

    Playermovement

--- Assets/Scripts/PlazerMovement.cs
+++ Assets/Scripts/PlazerMovement.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class PlazerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] float moveSpeed = 1f;
+    [SerializeField] Transform movePoint;
+
+    public LayerMask whatStopsMovement;
+
     void Start()
     {
-        
+        movePoint.parent = null;  
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
+
+        if(Vector3.Distance(transform.position, movePoint.position) <= 0.05f)
+        {
+            //Debug.Log("cLOSEeNOUGH");
+            if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f)
+            {
+                Debug.Log("Hori");
+                if (!Physics.CheckBox(movePoint.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f), new Vector3(0.2f, 0.2f, 0.2f), new Quaternion(0f, 0f, 0f, 0f), whatStopsMovement))
+                {
+                    movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
+                }
+            }
+            if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f)
+            {
+                if (!Physics.CheckBox(movePoint.position + new Vector3(0f, 0f, Input.GetAxisRaw("Vertical")), new Vector3(0.2f, 0.2f, 0.2f), new Quaternion(0f, 0f, 0f, 0f), whatStopsMovement))
+                {
+                    movePoint.position += new Vector3(0f, 0f, Input.GetAxisRaw("Vertical"));
+                }
+            }
+
+        }
     }
 }

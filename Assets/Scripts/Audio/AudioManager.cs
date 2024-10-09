9b571c1cfd6b23bb9f62cd194980c727cbc24050
Husa221 <dan.pluhar@seznam.cz>
2024-10-09 14:34:55 +0200

    fmodpackage + 1.SCRIPT

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance { get; private set; }
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogError("Found more than one AudioManager in the Scene");
+
+        }
+        instance = this;
+    }
+    public void PlayOneShot (EventReference sound, Vector3 worldPos)
+    {
+        RuntimeManager.PlayOneshot(sound, worldPos); 
+    }
+}
